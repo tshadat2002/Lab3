c2f77611ba5de9bedc6883a85510bdfefb671fad
tshadat2002 <tshadat2968@bths.edu>
2021-02-20 14:40:57 -0500

    Initial Commit

---
+++ Flashcard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcard
+//
+//  Created by Taohid Shadat on 2/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
